f3f6ae2c52f89ad04a6a67a2e7751ee4060d2125
Mark Isherwood <mark@markisherwood.me>
2018-11-30 22:07:41 +1100

    Set camera to only be active on scan tab.

--- amy/js/index.js
+++ amy/js/index.js
@@ -7,7 +7,8 @@ class PresentUnlocker {
   constructor(requiredShares) {
     this.requiredShares = requiredShares;
     this.shares = [];
-    const scanner_options = {
+    this.cameras;
+    const scannerOptions = {
       continuous: true,
       video: document.getElementById('js-camera-preview'),
       mirror: true,
@@ -15,23 +16,37 @@ class PresentUnlocker {
       refractoryPeriod: 5000,
       scanPeriod: 1,
     };
-    this.scanner = new Instascan.Scanner(scanner_options);
+    this.scanner = new Instascan.Scanner(scannerOptions);
     this.scanner.addListener('scan', (content) => {
       this.addShare(content);
-      this.changeTab('progress');
+      PresentUnlocker.changeTab('progress');
     });
     Instascan.Camera.getCameras().then((cameras) => {
-      if (cameras.length > 0) {
-        this.scanner.start(cameras[0]);
-      }
+      this.cameras = cameras;
     });
 
     this.updateCurrentScansPlaceholders();
     this.updateScansRequiredPlaceholders();
+
+    this.watchTabs();
   }
 
-  changeTab(tabName) {
-    var tab = document.getElementById(`${tabName}-tab`);
+  watchTabs() {
+    const tabLinks = document.querySelectorAll('a[data-toggle="tab"]');
+    tabLinks.forEach((tab) => {
+      tab.addEventListener('click', (event) => {
+        const target = event.target.getAttribute('href');
+        if (target === '#scan') {
+          this.scanner.start(this.cameras[0]);
+        } else {
+          this.scanner.stop();
+        }
+      });
+    });
+  }
+
+  static changeTab(tabName) {
+    const tab = document.getElementById(`${tabName}-tab`);
     tab.click();
   }
 
@@ -75,7 +90,7 @@ class PresentUnlocker {
   }
 }
 
-let present = new PresentUnlocker(10);
+const present = new PresentUnlocker(10);
 
 $('.navbar-nav>li>a').on('click', () => {
   $('.navbar-collapse').collapse('hide');
